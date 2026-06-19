7ff7b93de0e04886a8310483cf8906990ad91c26
Peter Steinberger <steipete@gmail.com>
2026-06-19 06:06:21 -0400

    fix: update Rovo provider registration

--- Sources/CodexBar/Providers/RovoDev/RovoDevProviderImplementation.swift
+++ Sources/CodexBar/Providers/RovoDev/RovoDevProviderImplementation.swift
@@ -1,9 +1,7 @@
 import AppKit
 import CodexBarCore
-import CodexBarMacroSupport
 import Foundation
 
-@ProviderImplementationRegistration
 struct RovoDevProviderImplementation: ProviderImplementation {
     let id: UsageProvider = .rovodev
 
